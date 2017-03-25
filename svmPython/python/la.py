852729bd11386571f18e01ff6e9b8f5207693808
metroid21 <G.G.LuisFer@gmail.com>
2017-03-25 14:07:31 -0600

    Comentarios en el codigo

--- svmPython/python/la.py
+++ svmPython/python/la.py
@@ -1,16 +1,33 @@
+#Libreria de svm/libsvm
 from svmutil import *
 
+#Cargar el archivo en las listas x y y
 y, x = svm_read_problem('../LINEAL_SEPARABLES_SVM')
+
+#Creacion de la lista de salidas con los primeros
+#100 de la primera y segunda clase
 salidas=y[0:100]+y[144:244]
+
+#Creacion de la lista de entradas con los primeros
+#100 de la primera y segunda clase
 entradas=x[0:100]+x[144:244]
+
+#Creacion de la lista de prueba con el resto de los datos
 test=x[100:143]+x[244:292]
+
+#Creación del vector de zeros
 zeros = [0] * len(test)
-m = svm_train(y, x, '-t 0 -s 1')
-p_label, p_acc, p_val = svm_predict(zeros, test, m)
 
-support_vector_coefficients = m.get_sv_coef()
-support_vectors = m.get_SV() 
+#Entrenamiento del svm con entrenamiento tipo lineal
+modelo = svm_train(y, x, '-t 0 -s 1')
 
-#print p_label
+#Prediccion de las clases de la lista test
+#e_finales = Etiquetas Finales
+#p_acc = accuracity/presicion
+#p_val = valores probables
+e_finales, p_acc, p_val = svm_predict(zeros, test, modelo)
+
+#Vectores soporte
+vS = modelo.get_SV() 
+
 print support_vectors
-#print support_vector_coefficients
